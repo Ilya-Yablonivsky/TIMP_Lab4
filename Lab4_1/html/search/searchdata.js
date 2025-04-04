5abe00b43dda0f4a2cbc99307887c1ee5f5ea077
Ilya <84633597+Ilya-Yablonivsky@users.noreply.github.com>
2025-04-04 10:00:17 +0300

    Add files via upload

---
+++ Lab4_1/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "ckm",
+  1: "ckm"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы"
+};
+
